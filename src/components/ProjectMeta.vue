48fdd54e40a0e858fbbcdd7e73cd38873830d35d
Tim Lauterwald <lauterwald@MacBook-Pro.local>
2021-12-27 13:31:46 +0100

    Update ProjectMeta.vue

--- src/components/ProjectMeta.vue
+++ src/components/ProjectMeta.vue
@@ -1,10 +1,5 @@
 <template>
   <div class="job-card-title">{{ title }}</div>
-  <div class="job-action">
-        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-          <path d="M20.8 4.6a5.5 5.5 0 00-7.7 0l-1.1 1-1-1a5.5 5.5 0 00-7.8 7.8l1 1 7.8 7.8 7.8-7.7 1-1.1a5.5 5.5 0 000-7.8z"></path></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><path d="M8.6 13.5l6.8 4M15.4 6.5l-6.8 4"></path></svg>
-  </div>
   <section class="project-meta">
     <h2 class="project-title">{{ title }}</h2>
     <ul class="project-categories">
