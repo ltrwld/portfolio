82b49afcdde32e9c2c7ad2793e9d870acfeec391
Tim Lauterwald <lauterwald@MacBook-Pro.local>
2021-12-27 22:03:14 +0100

    Update ProjectMeta.vue

--- src/components/ProjectMeta.vue
+++ src/components/ProjectMeta.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="job-subtitle-wrapper">
-    <h2 class="project-title">{{ title }}</h2>
-    <ul class="project-categories">
-      <li
-        class="project-category"
-        v-for="(category, index) in categories"
-        :key="index"
-      >
-        {{ category }}
-      </li>
-    </ul>
-    <div class="company-name">Patreon <span class="comp-location">{{ title }}, MD.</span></div>
-    <time class="posted" :datetime="year">Posted {{ year }} <span class="app-number">{{ title }}</span></time>
+  <div class="job-title-wrapper">
+    <div class="job-card-title">{{ title }}</div>
+    <div class="job-action">
+      <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M20.8 4.6a5.5 5.5 0 00-7.7 0l-1.1 1-1-1a5.5 5.5 0 00-7.8 7.8l1 1 7.8 7.8 7.8-7.7 1-1.1a5.5 5.5 0 000-7.8z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><path d="M8.6 13.5l6.8 4M15.4 6.5l-6.8 4"></path></svg>
+    </div>
   </div>
+  <div class="job-subtitle-wrapper">
+    <div class="company-name">Patreon <span class="comp-location">Londontowne, MD.</span></div>
+    <div class="posted">Posted 8 days ago<span class="app-number">98 Application</span></div>
+  </div>
+  
   <div class="explain-bar">
     <div class="explain-contents">
       <div class="explain-title">Experience</div>
