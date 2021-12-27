ce821be8fb004a4fe2ee5a023d3bc4a403af0905
Tim Lauterwald <lauterwald@MacBook-Pro.local>
2021-12-27 13:31:49 +0100

    Update Project.vue

--- src/templates/Project.vue
+++ src/templates/Project.vue
@@ -40,7 +40,11 @@
         :title="$page.project.title"
       />
        
-       
+       <div class="job-action">
+        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M20.8 4.6a5.5 5.5 0 00-7.7 0l-1.1 1-1-1a5.5 5.5 0 00-7.8 7.8l1 1 7.8 7.8 7.8-7.7 1-1.1a5.5 5.5 0 000-7.8z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><path d="M8.6 13.5l6.8 4M15.4 6.5l-6.8 4"></path></svg>
+       </div>
        </div>
        <div class="job-subtitle-wrapper">
         <div class="company-name">Patreon <span class="comp-location">Londontowne, MD.</span></div>
@@ -49,7 +53,10 @@
        <div class="explain-bar">
         <div class="explain-contents">
         <div class="explain-title">Experience</div>
-        <div class="explain-subtitle">Minimum 1 Year</div>
+        <ProjectMeta
+          class="explain-subtitle"
+          :title="$page.project.title"
+        />
          </div>
         <div class="explain-contents">
         <div class="explain-title">Work Level</div>
