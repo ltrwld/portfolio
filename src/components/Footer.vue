ac3a3b1ddc9ab556fa3ccdbe8e9b5da4c4ddbf8b
Tim Lauterwald <lauterwald@MacBook-Pro.local>
2021-12-27 01:19:26 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,8 @@
 <template>
   <footer class="footer" id="custom-footer">
-    <div class="to-top logo" @click="scrollTopTop"> 
-    
-    
-    
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 13.3 10.5" xml:space="preserve"><defs></defs><path d="M12.3,9.5H5.3C2.9,9.5,1,7.6,1,5.3V1h7.1c2.3,0,4.3,1.9,4.3,4.3V9.5z" class="st0"></path></svg>
-
- </div>
-    <div class="social">
+   
+    <div class="links">
       <a
-        class="channel"
         v-for="(social, index) in data.social"
         :key="index"
         :href="`https://${social.channel}.com/${social.handle}`"
@@ -17,15 +10,6 @@
         {{ social.link_text }}
       </a>
     </div>
-
-
-  
- 
-  <div class="links">
-    <a href="#">Terms & Conditions</a>
-    <a href="#">Contact</a>
-    <a href="#">Support</a>
-  </div>
   
 
   </footer>
@@ -39,14 +23,6 @@ export default {
     return {
       data
     };
-  },
-  methods: {
-    scrollTopTop() {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-      });
-    }
   }
 };
 </script>
@@ -59,32 +35,6 @@ export default {
   padding: 0 2rem;
   z-index: 100;
 }
-.to-top {
-  transform: rotate(-90deg);
-  user-select: none;
-  cursor: pointer;
-  border: 2px solid transparent;
-  text-align: center;
-  font-weight: 700;
-  line-height: 0;
-  padding: 1rem 0.5rem;
-  &:hover {
-    border: 2px solid white;
-  }
-}
-.social {
-  .channel {
-    margin-right: 1rem;
-    text-transform: uppercase;
-    letter-spacing: 0.05em;
-    &:last-of-type {
-      margin: 0;
-    }
-  }
-}
-
-
-
 
 
 
@@ -98,17 +48,7 @@ export default {
   color: #888;
   font-size: 14px;
 }
-footer .logo {
-  max-width: 150px;
-  display: inline-block;
-}
-footer .logo svg {
-  max-width: 100%;
-    width: 20px;
-    stroke: #fff;
-    fill: transparent;
-    stroke-width: 2;
-}
+
 #custom-footer > div {
   position: relative;
   top: 2px;
@@ -123,9 +63,7 @@ footer .logo svg {
   display: inline-block;
   margin-left: 20px;
 }
-#custom-footer .links a:hover {
-  color: #ddd;
-}
+
 #custom-footer .links a:first-of-type {
   margin-left: 0px;
 }
@@ -139,12 +77,6 @@ footer .logo svg {
   #custom-footer .links {
     margin-left: 0px;
   }
-  #custom-footer .meta {
-    float: left;
-  }
-  #custom-footer .logo {
-    display: none;
-  }
 }
 @media screen and (max-width: 600px) {
   #custom-footer {
@@ -152,16 +84,12 @@ footer .logo svg {
     font-size: 13px;
     line-height: 20px;
   }
-  #custom-footer .meta, #custom-footer .links {
+  #custom-footer .links {
     float: none;
   }
   #custom-footer .links {
     margin-bottom: 5px;
   }
-}
-
-a:hover {
-  text-decoration: none;
 }
 
 </style>
